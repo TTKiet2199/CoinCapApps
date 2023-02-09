6fc6b6eecec4b81ad071a62e73ba8c692de4737b
kiet <truongthekiet020199@gmail.com>
2023-02-09 16:35:54 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/coincapapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coincapapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CoinCap',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color.fromRGBO(88, 60, 197, 1.0),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
